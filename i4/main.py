e07fd1cdeb087435c3b986663d8d3a02311b3604
John Thomas <49878111+TohnJhomas@users.noreply.github.com>
2025-04-28 16:37:56 -0700

    Update main.py to include pros and cons

--- i4/main.py
+++ i4/main.py
@@ -13,6 +13,9 @@ def get_poetry_json(author):
         
     Returns:
         list: A list of dictionaries containing poetry data.
+
+    Pros: Consistent across many datasources. Easy to understand and use, especially once you're familiar with it. Lightweight and good at handling many small requestss
+    Cons: can be challenging to manage larger requests, especially with asynchronous or long-running endpoints. depending on API design it's often hard to handle things in bulk. Handling API keys and auth methods is tricky
     """
     url = f"https://poetrydb.org/author,poemcount/{author};5"
     response = requests.get(url)
@@ -29,6 +32,9 @@ def load_library_csv():
     
     Returns:
         pd.DataFrame: A DataFrame containing the library data.
+
+    Pros: Fast iteration time, once the data is downloaded it's much faster to load into a script or analysis tool. Everything is handled locally which removes certain barriers to troubleshooting as well. 
+    Cons: Storing data locally often leads to challenges with scaling when dealing with bigger datasets. Also it can be hard to enforce syntax rules and consistency across multiple files
     """
     script_dir = os.path.dirname(os.path.abspath(__file__))
     library_path = os.path.join(script_dir, "Seattle_Public_Library_5863745812754529831.csv")
@@ -37,6 +43,10 @@ def load_library_csv():
     return df
 
 def get_bigquery_data():
+    """
+    Pros: Robust set of features to perform complex analysis and nuanced queries, fast query processing time and, once set up, easy access to large amounts of data
+    Cons: challenging to set up and use at a small scale, slow iteration time for systems changes, steep learning curve. Costs money 
+    """
     client = bigquery.Client(project="erudite-canto-247700")
     query = "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` LIMIT 10"
     query_job = client.query(query)
